6ab6310f80f023a81a3264b326d6b598068cb465
MuthuBalaji98 <muthubalaji1998@gmail.com>
2022-09-11 21:11:10 +0530

    push

---
+++ main.js
@@ -0,0 +1,7 @@
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log("original array :",fruits);
+console.log("length of the array is "+fruits.length);
+console.log("method : array.push(kiwi)");
+fruits.push("Kiwi");
+console.log("change in array : ",fruits);
+console.log("return: length of array after push() is "+fruits.length);
